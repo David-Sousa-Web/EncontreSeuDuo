8b22e6e08515330d420caaabbf73942de90b5a33
David Sousa <david01sousa@Hotmail.com>
2022-09-13 18:33:45 -0300

    finalizando a parte statica da web

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./src/**/*.tsx",
+    "./index.html"
+
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['inter', 'sans-serif']
+    },
+    extend: {
+      colors:{
+      },
+      backgroundImage:{
+        galaxy: "url('/Fundo.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 29.08%, #43E7AD 70.94%, #E1D55D 18.57%)',
+        'game-gradiant': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      },
+    },
   },
   plugins: [],
 }
