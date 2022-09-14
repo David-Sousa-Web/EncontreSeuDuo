f14fe06b83581b1f23375cf37a057821b52723d7
David Sousa <david01sousa@Hotmail.com>
2022-09-14 20:22:24 -0300

    add: Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,30 @@ import express from "express";
 
 const app = express();
 
-app.get('/ads', (request, response) =>{
+app.get('/games', (request, response) =>{
+  return response.json([]);
+});
+
+app.post('/ads',(request, response) =>{
+  return response.status(201).json([])
+});
+
+app.get('/games/:id/ads', (request, response) =>{
+  //const gameid = request.parms.id
+
   return response.json([
     { id: 1, name: 'anuncio 1' },
     { id: 2, name: 'anuncio 2' },
     { id: 3, name: 'anuncio 3' }
   ])
-})
+});
+
+app.get('/games/:id/discord', (request, response) =>{
+  //const gameid = request.parms.id
+
+  return response.json([])
+});
+
+//localhost:3333
 
 app.listen(3333)
